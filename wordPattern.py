a3e972959420c3da402b12b31c47cf16d4797bbd
chengli731 <33361013+chengli731@users.noreply.github.com>
2018-03-16 23:45:16 -0400

    Update wordPattern.py

--- wordPattern.py
+++ wordPattern.py
@@ -1,8 +1,3 @@
-Python 3.6.0 (v3.6.0:41df79263a11, Dec 22 2016, 17:23:13) 
-[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
-Type "copyright", "credits" or "license()" for more information.
->>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
-Visit http://www.python.org/download/mac/tcltk/ for current information.
 class Solution(object):
     def wordPattern(self, pattern, str):
         """
@@ -13,17 +8,19 @@ class Solution(object):
         splitstr= str.split(" ")
         str_length = len(splitstr)
         pattern_length = len(pattern)
+        
+        # should take this else out as a sanity check
         if str_length != pattern_length:
             return False
-        else:
-            dic_pattern = {}
-            dic_str = {}
-            for i in range(str_length):
-                if pattern[i] not in dic_pattern and splitstr[i] not in dic_str:
-                    dic_pattern[pattern[i]] = splitstr[i]
-                    dic_str[splitstr[i]] = pattern[i]
-                if pattern[i] in dic_pattern and dic_pattern[pattern[i]]!= splitstr[i]:
-                    return False
-                if splitstr[i] in dic_str and dic_str[splitstr[i]]!= pattern[i]:
-                    return False
-            return True
+  
+        dic_pattern = {}
+        dic_str = {}
+        for i in range(str_length):
+             elif pattern[i] not in dic_pattern and splitstr[i] not in dic_str:
+                 dic_pattern[pattern[i]] = splitstr[i]
+                 dic_str[splitstr[i]] = pattern[i]
+             elif pattern[i] in dic_pattern and dic_pattern[pattern[i]]!= splitstr[i]:
+                 return False
+             elif splitstr[i] in dic_str and dic_str[splitstr[i]]!= pattern[i]:
+                 return False
+        return True

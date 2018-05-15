ffb93c78fe931304f56f7a1fac04161847de4437
chengli731 <33361013+chengli731@users.noreply.github.com>
2018-05-14 23:58:48 -0400

    Add files via upload

--- python/countprime.py
+++ python/countprime.py
@@ -4,15 +4,17 @@ class Solution:
         :type n: int
         :rtype: int
         """
+        if n <= 2:
+            return 0
         Pcount = [1]*(n+1)
         Pcount[0] = 0
         Pcount[1] = 0
         k = 2
-        while k^2 <= n:
+        while k^2 <= n-1:
             if Pcount[k] == 1:
                 j = k
-                while k*j <= n:
+                while k*j <= n-1:
                     Pcount[k*j] = 0
                     j+=1
             k+=1
-        return sum(Pcount)
+        return sum(Pcount)-1
